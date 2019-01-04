bc15b000cd4d607ac08fac56f94ca374d696a3e8
poem0207 <itachi971009@gmail.com>
2019-01-04 21:35:59 +0800

    HMM 前向算法

--- Machine_learning/HMM/hiddenMarkov.py
+++ Machine_learning/HMM/hiddenMarkov.py
@@ -2,6 +2,46 @@
 
 import numpy as np
 
+
+Q = [1, 2, 3]
+V = ['红', '白']
+A = [[0.5, 0.2, 0.3], [0.3, 0.5, 0.2], [0.2, 0.3, 0.5]]
+B = [[0.5, 0.5], [0.4, 0.6], [0.7, 0.3]]
+# O = ['红', '白', '红', '红', '白', '红', '白', '白']
+O = ['红', '白', '红']    #习题10.1的例子
+PI = [[0.2, 0.4, 0.4]]
+
 class HiddenMarkov:
     def forward(self,Q,V,A,B,O,PI):
-        
+        N = len(Q)     # 状态数量
+        M = len(O)     # 观测结果次数 = 时刻数量
+        T = M          # 时刻数量
+        alpha = np.zeros((M,N))    # alpha对应 任意时刻的任意状态
+        for t in range(T):
+            indexofO = V.index(O[t])     # 任意时刻的t对应的观测结果的索引  用于索引观测概率矩阵中结果概率位置
+            for i in range(N):   # 对于任意时刻下 处于的不同状态
+                B_t = B[i][indexofO]    # 该时刻该状态下 该观测结果的概率
+
+                if t == 0:  # 初始状态
+                    alpha[t][i] = PI[t][i] * B_t   # alpha 为到时刻t为止的前向概率
+
+                else:
+                    for j in range(N):
+                        alpha[t][i] += alpha[t-1][j]*A[j][i]*B_t   # 迭代前向算法
+        P = sum(alpha[T-1][i] for i in range(N) )
+
+        return P
+
+    def backward(self,Q,V,A,B,O,PI):
+        N = len(Q)
+        M = len(O)
+        betas = np.ones((M,N))
+        T = M
+        for t in range(M-2,-1,-1):
+
+
+
+
+HMM = HiddenMarkov()
+p = HMM.forward(Q,V,A,B,O,PI)
+print(p)
